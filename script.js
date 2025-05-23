81d78fa9c993165e549f787d7f81ea8582031f39
Miguel Angel Acosta <angelacostajr@outlook.com>
2025-05-23 12:56:15 -0500

    Lista_menú-mobil

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+(function(){
+    const list = document.querySelector('.nav-links');
+    const menu = document.querySelector('.menu-ico');
 
+    menu.addEventListener('click', () => {
+        list.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+
+    list.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            if(list.classList.contains('active')) {
+                list.classList.remove('active');
+                menu.classList.remove('active');
+            }});
+
+})
 document.addEventListener('DOMContentLoaded', function () {
 
     console.log('Aulalink cargado!');
-});
+});})();
